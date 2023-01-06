75dd5eacef612701c2a799392d885c2d138458ce
soohofather <enthchal@gmail.com>
2023-01-07 04:37:08 +0900

    동현 : 글쓰기 기능 구현

--- articles/models.py
+++ articles/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Article(models.Model):
+    title = models.CharField(max_length=20)
+    content = models.TextField()
+    incoming = models.PositiveIntegerField(default=0)  # 음수일 수 없음
+    spending = models.PositiveIntegerField(default=0)  # 음수일 수 없음
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
