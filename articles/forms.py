4bbaacc78070db6b4fd42f16785e4a638f7be821
soohofather <enthchal@gmail.com>
2023-01-07 19:25:55 +0900

    동현 : 날짜 입력, 총 수입, 총 지출, 본인 게시글만 노출 구현

--- articles/forms.py
+++ articles/forms.py
@@ -2,7 +2,19 @@ from django import forms
 from .models import Article
 
 
+class DateInput(forms.DateInput):
+    input_type = "date"
+
+
 class ArticleForm(forms.ModelForm):
     class Meta:
         model = Article
-        fields = ["title", "content", "incoming", "spending"]
+        fields = ["date", "title", "content", "incoming", "spending"]
+        widgets = {"date": DateInput()}
+        labels = {
+            "date": "날짜",
+            "title": "내용",
+            "content": "메모",
+            "incoming": "수입",
+            "spending": "지출",
+        }
--- articles/models.py
+++ articles/models.py
@@ -1,12 +1,14 @@
 from django.db import models
 from config import settings
+import datetime
 
 # Create your models here.
 
 
 class Article(models.Model):
+    date = models.DateField(("Date"), default=datetime.date.today)
     title = models.CharField(max_length=20)
-    content = models.TextField()
+    content = models.TextField(blank=True)
     incoming = models.PositiveIntegerField(default=0)  # 음수일 수 없음
     spending = models.PositiveIntegerField(default=0)  # 음수일 수 없음
     created_at = models.DateTimeField(auto_now_add=True)
