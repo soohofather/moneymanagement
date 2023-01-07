e41781524983e9adde79be530cc065c7349d161d
soohofather <enthchal@gmail.com>
2023-01-07 20:51:09 +0900

    동현 : SQLite에서 MYSQL로 데이터베이스 변경

--- config/settings.py
+++ config/settings.py
@@ -76,13 +76,18 @@ WSGI_APPLICATION = "config.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
+# 기존 SQLite
+# DATABASES = {
+#      "default": {
+#          "ENGINE": "django.db.backends.sqlite3",
+#          "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+#      }
+# }
 
+# MYSQL 변경
+import my_settings
+
+DATABASES = my_settings.DATABASES
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
